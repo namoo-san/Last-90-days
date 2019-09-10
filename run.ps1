c58461b3f5dd9a36895d2c459d991acc9cee8fee
namoo-san <highgraphics.x@gmail.com>
2019-09-10 13:08:43 +0900

    Modified Remove-ADComputer to Remove-ADObject

--- run.ps1
+++ run.ps1
@@ -1,6 +1,7 @@
 # Remove "Computer Account" from Active Directory, that has not been logged in for the past 90 days.
 # Author : namoo-san
 # Reference : https://docs.microsoft.com/en-us/powershell/module/addsadministration/remove-adcomputer?view=win10-ps
+# Reference : https://social.technet.microsoft.com/Forums/ja-JP/bad65781-994d-48b5-bd88-2bfe80106aa5/removeadcomputer?forum=powershellja
 
 # Set variables
 $ExpireDate = (Get-Date).AddDays(-90)
@@ -17,8 +18,18 @@ Search-ADAccount -AccountInactive -DateTime $ExpireDate -ComputersOnly | Format-
 Import-Module ActiveDirectory
 $f = (Get-Content $ExportFile) -as [string[]]
 $i=1
-foreach ($l in $f) {
-    Write-Host $l
-    Remove-ADComputer -Identity $l.TrimEnd()
+foreach ($Objects in $f) {
+    $HostName = $objects.TrimEnd()
+    
+    # Remove messages
+    Write-Output "$HostName remove from Active Directory."
+    try {
+        # Remove-ADComputer -Identity $l.TrimEnd() -Confirm:$False | Out-File $ResultExport -Encoding UTF8 -Append
+        Get-ADComputer $HostName.TrimEnd() | Remove-ADObject -Recursive -Confirm:$False
+        Write-Output "$HostName removed."
+    }
+    catch {
+        Write-Output "$HostName remove failed..."
+    }
     $i++
 }
